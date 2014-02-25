04cab25fec6d5a46961b58831802d152ec56eb76
python1320 <python1320@8ee87cb0-0e4a-4ddb-82c9-103423a9051a>
2014-02-25 20:04:58 +0000

    no more luasocket_stuff.luaopen_socket_core() Why was it originally like this though?

--- luasocket/src/main.c
+++ luasocket/src/main.c
@@ -1,11 +1,6 @@
-//#include "GMLuaModule.h"
-
-
 #include "lua.h"
 int luaopen_socket_core(lua_State *L);
 int luaopen_mime_core(lua_State *L);
-
-
 
 #ifdef _WIN32
 	#define GLUA_DLL_EXPORT  __declspec( dllexport ) 
@@ -13,17 +8,23 @@ int luaopen_mime_core(lua_State *L);
 	#define GLUA_DLL_EXPORT	 __attribute__((visibility("default"))) 
 #endif
 
-
+int luaopen_dummy_func(lua_State *L)  {
+	return 0;
+}
 
 GLUA_DLL_EXPORT int gmod13_open( lua_State* L )
 {
 	lua_newtable(L);
-	lua_pushcfunction(L, luaopen_socket_core);
+	lua_pushcfunction(L, luaopen_dummy_func);
 	lua_setfield(L, -2, "luaopen_socket_core");
-	lua_pushcfunction(L, luaopen_mime_core);
+	lua_pushcfunction(L, luaopen_dummy_func);
 	lua_setfield(L, -2, "luaopen_mime_core");
-	lua_setglobal(L, "luasocket_stuff"); /* ugly hack end */
-
+	lua_setglobal(L, "luasocket_stuff");
+	
+	// let's just load it directly?
+	luaopen_socket_core(L);
+	luaopen_mime_core(L);
+	
 	return 0;
 }
 
