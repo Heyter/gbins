21bab635474ae51e5732b35813a8336ea46eec1e
python1320 <python1320@8ee87cb0-0e4a-4ddb-82c9-103423a9051a>
2014-02-25 19:16:03 +0000

    Linux!

--- luasocket/premake.lua
+++ luasocket/premake.lua
@@ -16,12 +16,12 @@ SOLUTION"luasocket"
 		links		{"Ws2_32","lua_shared"}
 		
 	LINUX()
-		links		{"lua_shared_srv"}
 
 	PROJECT()
 		--INCLUDES	"lua51"
-		files		{
-						"src/*.c",
+		files		{"src/*.c"}
+		configuration	("windows")
+			files	{
 						"luasocket/src/auxiliar.c",
 						"luasocket/src/buffer.c",
 						"luasocket/src/except.c",
@@ -35,4 +35,27 @@ SOLUTION"luasocket"
 						"luasocket/src/timeout.c",
 						"luasocket/src/udp.c",
 						"luasocket/src/wsocket.c",
+					}
+		configuration	("linux")
+			
+			-- OH MY GOD PLEASE DONT LOOK
+			linkoptions		{"-Wl,-rpath='$$ORIGIN'"}
+			linkoptions		{"garrysmod/bin/lua_shared_srv.so"}
+			prelinkcommands	{"mkdir -p garrysmod/bin && ln -s "..SRCDS_DIR.."/garrysmod/bin/lua_shared_srv.so garrysmod/bin/lua_shared_srv.so "}
+			
+			files	{
+						"luasocket/src/auxiliar.c",
+						"luasocket/src/buffer.c",
+						"luasocket/src/except.c",
+						"luasocket/src/inet.c",
+						"luasocket/src/io.c",
+						"luasocket/src/luasocket.c",
+						"luasocket/src/mime.c",
+						"luasocket/src/options.c",
+						"luasocket/src/select.c",
+						"luasocket/src/tcp.c",
+						"luasocket/src/timeout.c",
+						"luasocket/src/udp.c",
+						"luasocket/src/unix.c",
+						"luasocket/src/usocket.c",
 					}
