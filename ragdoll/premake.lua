aaccadcb90ea285b93b3e302772db8ceb642975f
python1320 <python1320@8ee87cb0-0e4a-4ddb-82c9-103423a9051a>
2014-03-03 17:23:08 +0000

    initial build script

---
+++ ragdoll/premake.lua
@@ -0,0 +1,19 @@
+dofile("../common.lua")
+
+RequireDefaultlibs()
+
+SOLUTION"ragdoll"
+	
+	targetdir	"Release"
+	INCLUDES	"source_sdk"
+	INCLUDES	"gmod_sdk"
+	defines		{"NDEBUG"}
+	
+	WINDOWS()
+	LINUX()
+
+	PROJECT()
+		SOURCE_SDK_LINKS()
+		configuration 		"windows"
+			--links	"steam_api"
+		configuration 		"linux"
