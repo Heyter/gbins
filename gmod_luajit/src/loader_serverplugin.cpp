435ccc9ff8af2320e1154263a1c9ab3dbccc10b8
python1320 <python1320@8ee87cb0-0e4a-4ddb-82c9-103423a9051a>
2014-07-02 16:53:54 +0000

    gmod luajit because some day we might want to override lua_shared from https://dl.dropboxusercontent.com/u/20789739/gmod_luajit.rar

---
+++ gmod_luajit/src/loader_serverplugin.cpp
@@ -0,0 +1,210 @@
+//===== Copyright © 1996-2008, Valve Corporation, All rights reserved. ======//
+//
+// Purpose: 
+//
+// $NoKeywords: $
+//
+//===========================================================================//
+
+#include "StdAfx.h"
+#include "luareplace.h"
+
+// Uncomment this to compile the sample TF2 plugin code, note: most of this is duplicated in serverplugin_tony, but kept here for reference!
+//#define SAMPLE_TF2_PLUGIN
+// memdbgon must be the last include file in a .cpp file!!!
+#include "tier0/memdbgon.h"
+
+CGlobalVars *gpGlobals = NULL;
+
+//---------------------------------------------------------------------------------
+// Purpose: a sample 3rd party plugin class
+//---------------------------------------------------------------------------------
+class CLuaReplace: public IServerPluginCallbacks
+{
+public:
+	CLuaReplace();
+	~CLuaReplace();
+
+	// IServerPluginCallbacks methods
+	virtual bool			Load(	CreateInterfaceFn interfaceFactory, CreateInterfaceFn gameServerFactory );
+	virtual void			Unload( void );
+	virtual void			Pause( void );
+	virtual void			UnPause( void );
+	virtual const char     *GetPluginDescription( void );      
+	virtual void			LevelInit( char const *pMapName );
+	virtual void			ServerActivate( edict_t *pEdictList, int edictCount, int clientMax );
+	virtual void			GameFrame( bool simulating );
+	virtual void			LevelShutdown( void );
+	virtual void			ClientActive( edict_t *pEntity );
+	virtual void			ClientDisconnect( edict_t *pEntity );
+	virtual void			ClientPutInServer( edict_t *pEntity, char const *playername );
+	virtual void			SetCommandClient( int index );
+	virtual void			ClientSettingsChanged( edict_t *pEdict );
+	virtual PLUGIN_RESULT	ClientConnect( bool *bAllowConnect, edict_t *pEntity, const char *pszName, const char *pszAddress, char *reject, int maxrejectlen );
+	virtual PLUGIN_RESULT	ClientCommand( edict_t *pEntity, const CCommand &args );
+	virtual PLUGIN_RESULT	NetworkIDValidated( const char *pszUserName, const char *pszNetworkID );
+	virtual void			OnQueryCvarValueFinished( QueryCvarCookie_t iCookie, edict_t *pPlayerEntity, EQueryCvarValueStatus eStatus, const char *pCvarName, const char *pCvarValue );
+
+	// added with version 3 of the interface.
+	virtual void			OnEdictAllocated( edict_t *edict );
+	virtual void			OnEdictFreed( const edict_t *edict  );	
+};
+
+
+// 
+// The plugin is a static singleton that is exported as an interface
+//
+CLuaReplace g_LuaReplace;
+EXPOSE_SINGLE_INTERFACE_GLOBALVAR(CLuaReplace, IServerPluginCallbacks, INTERFACEVERSION_ISERVERPLUGINCALLBACKS, g_LuaReplace );
+
+//---------------------------------------------------------------------------------
+// Purpose: constructor/destructor
+//---------------------------------------------------------------------------------
+CLuaReplace::CLuaReplace()
+{
+}
+
+CLuaReplace::~CLuaReplace()
+{
+}
+
+//---------------------------------------------------------------------------------
+// Purpose: called when the plugin is loaded, load the interface we need from the engine
+//---------------------------------------------------------------------------------
+bool CLuaReplace::Load(	CreateInterfaceFn interfaceFactory, CreateInterfaceFn gameServerFactory )
+{
+	Init();
+
+	return true;
+}
+
+//---------------------------------------------------------------------------------
+// Purpose: called when the plugin is unloaded (turned off)
+//---------------------------------------------------------------------------------
+void CLuaReplace::Unload( void )
+{
+	Shutdown();
+}
+
+//---------------------------------------------------------------------------------
+// Purpose: called when the plugin is paused (i.e should stop running but isn't unloaded)
+//---------------------------------------------------------------------------------
+void CLuaReplace::Pause( void )
+{
+}
+
+//---------------------------------------------------------------------------------
+// Purpose: called when the plugin is unpaused (i.e should start executing again)
+//---------------------------------------------------------------------------------
+void CLuaReplace::UnPause( void )
+{
+}
+
+//---------------------------------------------------------------------------------
+// Purpose: the name of this plugin, returned in "plugin_print" command
+//---------------------------------------------------------------------------------
+const char *CLuaReplace::GetPluginDescription( void )
+{
+	return "Garry's Mod LuaJIT";
+}
+
+//---------------------------------------------------------------------------------
+// Purpose: called on level start
+//---------------------------------------------------------------------------------
+void CLuaReplace::LevelInit( char const *pMapName )
+{
+}
+
+//---------------------------------------------------------------------------------
+// Purpose: called on level start, when the server is ready to accept client connections
+//		edictCount is the number of entities in the level, clientMax is the max client count
+//---------------------------------------------------------------------------------
+void CLuaReplace::ServerActivate( edict_t *pEdictList, int edictCount, int clientMax )
+{
+}
+
+//---------------------------------------------------------------------------------
+// Purpose: called once per server frame, do recurring work here (like checking for timeouts)
+//---------------------------------------------------------------------------------
+void CLuaReplace::GameFrame( bool simulating )
+{
+}
+
+//---------------------------------------------------------------------------------
+// Purpose: called on level end (as the server is shutting down or going to a new map)
+//---------------------------------------------------------------------------------
+void CLuaReplace::LevelShutdown( void ) // !!!!this can get called multiple times per map change
+{
+}
+
+//---------------------------------------------------------------------------------
+// Purpose: called when a client spawns into a server (i.e as they begin to play)
+//---------------------------------------------------------------------------------
+void CLuaReplace::ClientActive( edict_t *pEntity )
+{
+}
+
+//---------------------------------------------------------------------------------
+// Purpose: called when a client leaves a server (or is timed out)
+//---------------------------------------------------------------------------------
+void CLuaReplace::ClientDisconnect( edict_t *pEntity )
+{
+}
+
+//---------------------------------------------------------------------------------
+// Purpose: called on 
+//---------------------------------------------------------------------------------
+void CLuaReplace::ClientPutInServer( edict_t *pEntity, char const *playername )
+{
+}
+
+//---------------------------------------------------------------------------------
+// Purpose: called on level start
+//---------------------------------------------------------------------------------
+void CLuaReplace::SetCommandClient( int index )
+{
+}
+
+//---------------------------------------------------------------------------------
+// Purpose: called on level start
+//---------------------------------------------------------------------------------
+void CLuaReplace::ClientSettingsChanged( edict_t *pEdict )
+{
+}
+
+//---------------------------------------------------------------------------------
+// Purpose: called when a client joins a server
+//---------------------------------------------------------------------------------
+PLUGIN_RESULT CLuaReplace::ClientConnect( bool *bAllowConnect, edict_t *pEntity, const char *pszName, const char *pszAddress, char *reject, int maxrejectlen )
+{
+	return PLUGIN_CONTINUE;
+}
+
+//---------------------------------------------------------------------------------
+// Purpose: called when a client types in a command (only a subset of commands however, not CON_COMMAND's)
+//---------------------------------------------------------------------------------
+PLUGIN_RESULT CLuaReplace::ClientCommand( edict_t *pEntity, const CCommand &args )
+{
+	return PLUGIN_CONTINUE;
+}
+
+//---------------------------------------------------------------------------------
+// Purpose: called when a client is authenticated
+//---------------------------------------------------------------------------------
+PLUGIN_RESULT CLuaReplace::NetworkIDValidated( const char *pszUserName, const char *pszNetworkID )
+{
+	return PLUGIN_CONTINUE;
+}
+
+//---------------------------------------------------------------------------------
+// Purpose: called when a cvar value query is finished
+//---------------------------------------------------------------------------------
+void CLuaReplace::OnQueryCvarValueFinished( QueryCvarCookie_t iCookie, edict_t *pPlayerEntity, EQueryCvarValueStatus eStatus, const char *pCvarName, const char *pCvarValue )
+{
+}
+void CLuaReplace::OnEdictAllocated( edict_t *edict )
+{
+}
+void CLuaReplace::OnEdictFreed( const edict_t *edict  )
+{
+}
---
+++ gmod_luajit/src/luareplace.h
@@ -0,0 +1,2 @@
+void Init();
+void Shutdown();
