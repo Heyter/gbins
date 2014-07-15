fec63c589e972b5fb10d3240124b30f314ec41dd
python1320 <python1320@8ee87cb0-0e4a-4ddb-82c9-103423a9051a>
2014-07-15 22:05:16 +0000

    1:04 - MetaMan: FIX YOUR SHIT FFS

--- luacurl/3_getluacurl.cmd
+++ luacurl/3_getluacurl.cmd
@@ -1,6 +1,6 @@
 @call ../settings.cmd
 
-wget --no-check-certificate https://github.com/msva/lua-curl/archive/master.zip
+wget --no-check-certificate https://github.com/msva/lua-curl/archive/master.zip -O master.zip
 unzip master.zip
 del master.zip
 move lua-curl-master lua-curl
--- luacurl/getluacurl.sh
+++ luacurl/getluacurl.sh
@@ -1,4 +1,4 @@
-wget --no-check-certificate https://github.com/msva/lua-curl/archive/master.zip
+wget --no-check-certificate https://github.com/msva/lua-curl/archive/master.zip -O master.zip
 unzip master.zip
 rm master.zip
 mv lua-curl-master lua-curl
