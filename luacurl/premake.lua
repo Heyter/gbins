5fc216bc482795674e5a825bc30543f3b0507400
python1320 <python1320@8ee87cb0-0e4a-4ddb-82c9-103423a9051a>
2014-07-15 22:24:06 +0000

    yeah screw default target

--- luacurl/premake.lua
+++ luacurl/premake.lua
@@ -15,14 +15,15 @@ SOLUTION"luacurl"
 	LINUX()
 
 	PROJECT()
-		files	{"src/*.c"}
 		files	{"lua-curl/src/*.c"}
-		includedirs	"curl-src/builds/libcurl-vc12-x86-release-dll-zlib-dll-ipv6-sspi-spnego-winssl/include"
-		libdirs	"curl-src/builds/libcurl-vc12-x86-release-dll-zlib-dll-ipv6-sspi-spnego-winssl/lib"
-		links	"libcurl"
+		files	{"src/*.c"}
 		INCLUDES	"lua51"
 		
 		configuration	("windows")
+			includedirs	"curl-src/builds/libcurl-vc12-x86-release-dll-zlib-dll-ipv6-sspi-spnego-winssl/include"
+			libdirs	"curl-src/builds/libcurl-vc12-x86-release-dll-zlib-dll-ipv6-sspi-spnego-winssl/lib"
+			links	"libcurl"
 			defines "DISABLE_INVALID_NUMBERS"
 		configuration	("linux")
-		
+			links	"curl"
+			
