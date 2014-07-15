258cf720cd1fda158daea26b8d1bcacc75a235ca
python1320 <python1320@8ee87cb0-0e4a-4ddb-82c9-103423a9051a>
2014-07-15 22:02:13 +0000

    1:01 - MetaMan: fix your cds 1:01 - MetaMan: asshole

--- luacurl/2_curl.cmd
+++ luacurl/2_curl.cmd
@@ -6,5 +6,6 @@ del curl-7.37.0.zip
 move curl-7.37.0 curl-src
 cd curl-src\winbuild
 nmake -f Makefile.vc mode=dll VC=12 WITH_ZLIB=dll ENABLE_WINSSL=yes ENABLE_IDN=yes ENABLE_IPV6=yes ENABLE_SSPI=yes ENABLE_SPNEGO=yes DEBUG=no MACHINE=x86
+cd ..\..
 pause
 
