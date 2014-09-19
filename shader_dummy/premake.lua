873b6c2450928034885813062db65e08f56aac22
python1320 <python1320@8ee87cb0-0e4a-4ddb-82c9-103423a9051a>
2014-09-19 22:07:29 +0000

    out

--- shader_dummy/premake.lua
+++ shader_dummy/premake.lua
@@ -5,9 +5,6 @@ RequireDefaultlibs()
 SOLUTION"game_shader_generic_garrysmod_srv"
 	
 	INCLUDES	"source_sdk"
---	INCLUDES	"hooking"			
---	INCLUDES	"luajit"
---	INCLUDES	"sigscanning"
 	defines		{"PRELOADPLUGIN","GMMODULE","GAME_DLL"}
 	
 	WINDOWS()
@@ -16,21 +13,11 @@ SOLUTION"game_shader_generic_garrysmod_srv"
 	PROJECT()
 		SOURCE_SDK_LINKS()
 		
---		INCLUDES	"luajit"
-
 	        targetprefix                    ""
 		
 		configuration 		"windows"
-			--defines 	"__WIN32__"
---			files 		"src/detours/*.cpp"
---			includedirs "src/detours"
         	        targetsuffix            ""
 		configuration 		"linux"
---			links	{"luajit","dl","rt","m","pthread","c"}
---			defines {"_GNU_SOURCE","GNU_SOURCE"}
 	                targetsuffix            ""
 	                targetextension         ".so"
-			--linkoptions		{"-Wl,-rpath='$$ORIGIN'"}
-			--linkoptions		{"garrysmod/bin/server_srv.so"}
-			--prelinkcommands	{"mkdir -p garrysmod/bin && ln -fs "..SRCDS_DIR.."/garrysmod/bin/server_srv.so garrysmod/bin/server_srv.so "}
-			
+					
--- shader_dummy/src/StdAfx.h
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-
-#include <engine/iserverplugin.h>
-#include <materialsystem/IShader.h>
-#include <tier2/tier2.h>
