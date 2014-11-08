e5670d531ea13f44f5cdf33063562edd795152d5
agent47 <agent47@8ee87cb0-0e4a-4ddb-82c9-103423a9051a>
2014-11-08 20:12:50 +0000

    Added dbghelper module. Adds a debug library method that allows (hopefully) yielding on coroutines with debug.sethook events.

--- dump_lua_shared.cmd
+++ dump_lua_shared.cmd
@@ -3,7 +3,7 @@
 @cd lua51
 @echo Dumping %DLLPATH% 
 @echo EXPORTS > lua_shared.def
-@dumpbin /EXPORTS "%DLLPATH%" | grep -A 99999 "ordinal hint" | tail +3 | grep -B 9999 Summary | head -n -2 | awk "{print $4}" >>  lua_shared.def
+@dumpbin /EXPORTS "%DLLPATH%" | grep -A 99999 "ordinal hint" | tail -n +3 | grep -B 9999 Summary | head -n -2 | awk "{print $4}" >>  lua_shared.def
 @lib /MACHINE:x86 /def:lua_shared.def /OUT:lua_shared.lib
 @cp lua_shared.lib lua51.lib
 @pause
