a30311811f8f9eaab377c2e26408618de0692351
python1320 <python1320@8ee87cb0-0e4a-4ddb-82c9-103423a9051a>
2015-03-28 17:28:51 +0000

    test2

--- fpu/src/main.cpp
+++ fpu/src/main.cpp
@@ -14,30 +14,43 @@
 #include <cstdio>
 #include <GarrysMod/Lua/Interface.h>
 
+static unsigned short
+x87_get_control_word(void)
+{
+    
+
+	static unsigned short mode;
+    #ifdef _WIN32 
+		__asm {
+			fstcw mode
+		} 
+    
+	#else
+		__asm__ __volatile__("fstcw %0\n\t":"=m"(*&mode):);
+    #endif 
+	return mode;
+}
+
 
 GMOD_MODULE_OPEN( )
 {
 
     double a = 0.1;
     float f = 0.1;
-    unsigned int control_word;
-    int err;
+	double aa = 0.1111111111111111111111111;
+    float ff = 0.111111111111111111111111;
+	double aaa = 0.66666666666666666666666;
+    float fff = 0.666666666666666666666666;
+	
+    unsigned short control_word;
 
-  
-	#ifdef _WIN32
-		// Show original FP control word and do calculation.
-		err = _controlfp_s(&control_word, 0, 0);
-		if ( err ) {
-			printf( "fpu error %d\n",err);
-			
-		}
-	#else
-		_FPU_GETCW(control_word);
-	#endif
+	control_word = x87_get_control_word();
 
-    printf( "FPU Control Word: 0x%.4x\n", control_word );
+    printf( "FPU Control Word: %.4x\n", control_word );
     printf( "%1.1f * %1.1f = %.15e\n", a, a, a * a );
-    printf( "%1.1f * %1.1f = %.15e\n", f, f, f * f );
+	
+	printf( "conv %.15e (%.15e) -> %.15e\n", aa,ff,(float)aa );
+	printf( "conv %.15e (%.15e) -> %.15e\n", aaa,fff,(float)aaa );
 	
 	return 0;
 }
