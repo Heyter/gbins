60b134c60847f325b32aa0de36f7fd0179390a51
python1320 <python1320@8ee87cb0-0e4a-4ddb-82c9-103423a9051a>
2015-06-22 16:58:16 +0000

    not ehre

--- physsim/src/CLASS_LuaBoundFunction.cpp
+++
@@ -1,9 +0,0 @@
-#include "CLASS_LuaBoundFunction.h"
-
-LuaBoundFunction::LuaBoundFunction(const char* name, GarrysMod::Lua::CFunc fptr, bool meta)
-	: m_functionName(name)
-	, m_function(fptr)
-	, m_isMetaFunction(meta)
-{
-}
-
