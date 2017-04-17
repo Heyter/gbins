cb5d4d6a5d63b3e962926640e5f01623d3e9e204
python1320 <python1320@8ee87cb0-0e4a-4ddb-82c9-103423a9051a>
2017-04-17 17:20:12 +0000

    revert (this is dev version)

--- gmod-module-base/include/GarrysMod/Lua/Types.h
+++ gmod-module-base/include/GarrysMod/Lua/Types.h
@@ -1,129 +1,112 @@
+
 #ifndef GARRYSMOD_LUA_TYPES_H
 #define GARRYSMOD_LUA_TYPES_H
 
 #ifdef ENTITY
 #undef ENTITY
-#endif
+#endif 
 
 #ifdef VECTOR
 #undef VECTOR
-#endif
+#endif 
 
-namespace GarrysMod
+namespace GarrysMod 
 {
-    namespace Lua
-    {
-        namespace Type
-        {
-            enum
-            {
-#ifdef GMOD_ALLOW_DEPRECATED
-                // Deprecated: Use NONE instead of INVALID
-                INVALID = -1,
-#endif
+	namespace Lua
+	{
+		namespace Type
+		{
+			enum
+			{
 
-                // Lua Types
-                NONE = -1,
-                NIL,
-                BOOL,
-                LIGHTUSERDATA,
-                NUMBER,
-                STRING,
-                TABLE,
-                FUNCTION,
-                USERDATA,
-                THREAD,
+				INVALID = -1,
+				NIL, 
+				BOOL,
+				LIGHTUSERDATA,
+				NUMBER, 
+				STRING, 
+				TABLE,
+				FUNCTION,
+				USERDATA,
+				THREAD,
 
-                // GMod Types
-                ENTITY,
-                VECTOR,
-                ANGLE,
-                PHYSOBJ,
-                SAVE,
-                RESTORE,
-                DAMAGEINFO,
-                EFFECTDATA,
-                MOVEDATA,
-                RECIPIENTFILTER,
-                USERCMD,
-                SCRIPTEDVEHICLE,
-                MATERIAL,
-                PANEL,
-                PARTICLE,
-                PARTICLEEMITTER,
-                TEXTURE,
-                USERMSG,
-                CONVAR,
-                IMESH,
-                MATRIX,
-                SOUND,
-                PIXELVISHANDLE,
-                DLIGHT,
-                VIDEO,
-                FILE,
-                LOCOMOTION,
-                PATH,
-                NAVAREA,
-                SOUNDHANDLE,
-                NAVLADDER,
-                PARTICLESYSTEM,
-                PROJECTEDTEXTURE,
+				// UserData
+				ENTITY, 
+				VECTOR, 
+				ANGLE,
+				PHYSOBJ,
+				SAVE,
+				RESTORE,
+				DAMAGEINFO,
+				EFFECTDATA,
+				MOVEDATA,
+				RECIPIENTFILTER,
+				USERCMD,
+				SCRIPTEDVEHICLE,
 
-                COUNT
-            };
+				// Client Only
+				MATERIAL,
+				PANEL,
+				PARTICLE,
+				PARTICLEEMITTER,
+				TEXTURE,
+				USERMSG,
 
-#if ( defined( GMOD ) || defined( GMOD_ALLOW_DEPRECATED ) )
-            // You should use ILuaBase::GetTypeName instead of directly accessing this array
-            static const char* Name[] =
-            {
-                "nil",
-                "bool",
-                "lightuserdata",
-                "number",
-                "string",
-                "table",
-                "function",
-                "userdata",
-                "thread",
-                "entity",
-                "vector",
-                "angle",
-                "physobj",
-                "save",
-                "restore",
-                "damageinfo",
-                "effectdata",
-                "movedata",
-                "recipientfilter",
-                "usercmd",
-                "vehicle",
-                "material",
-                "panel",
-                "particle",
-                "particleemitter",
-                "texture",
-                "usermsg",
-                "convar",
-                "mesh",
-                "matrix",
-                "sound",
-                "pixelvishandle",
-                "dlight",
-                "video",
-                "file",
-                "locomotion",
-                "path",
-                "navarea",
-                "soundhandle",
-                "navladder",
-                "particlesystem",
-                "projectedtexture",
+				CONVAR,
+				IMESH,
+				MATRIX,
+				SOUND,
+				PIXELVISHANDLE,
+				DLIGHT,
+				VIDEO,
+				FILE,
 
-                0
-            };
-#endif
-        }
-    }
+				COUNT
+			};
+
+			static const char* Name[] = 
+			{
+				"nil",
+				"bool",
+				"lightuserdata",
+				"number",
+				"string",
+				"table",
+				"function",
+				"userdata",
+				"thread",
+				"entity",
+				"vector",
+				"angle",
+				"physobj",
+				"save",
+				"restore",
+				"damageinfo",
+				"effectdata",
+				"movedata",
+				"recipientfilter",
+				"usercmd",
+				"vehicle",
+				"material",
+				"panel",
+				"particle",
+				"particleemitter",
+				"texture",
+				"usermsg",
+				"convar",
+				"mesh",
+				"matrix",
+				"sound",
+				"pixelvishandle",
+				"dlight",
+				"video",
+				"file",
+
+				0
+			};
+		}
+	}
 }
 
-#endif
+#endif 
+
