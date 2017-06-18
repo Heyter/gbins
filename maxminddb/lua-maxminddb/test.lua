85c4d9e138e5b4443a63c1ba17081a917c82986e
python1320 <python1320@8ee87cb0-0e4a-4ddb-82c9-103423a9051a>
2017-06-18 21:47:01 +0000

    doe snot belong in the repo

--- maxminddb/lua-maxminddb/test.lua
+++
@@ -1,5 +0,0 @@
-local mm = require 'maxminddb'
-local db = mm.open('/var/lib/libmaxminddb/GeoLite2-City.mmdb')
-
-local res = db:lookup('8.8.8.8')
-print(res:get("country", "names", "en"), res:get("location", "longitude"), res:get("location", "latitude"))
