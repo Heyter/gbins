50f86ec24c2fc1b6ded8ea157bc906bd5fd94ae3
SpiralP <SpiralP@8ee87cb0-0e4a-4ddb-82c9-103423a9051a>
2018-04-27 21:42:47 +0000

    gsapi: fix steam_api.lib linker path issue where we want to use steamworks' first

--- gsapi/premake.lua
+++ gsapi/premake.lua
@@ -1,25 +1,42 @@
---DEBUG=true
 dofile("../common.lua")
 
 --RequireDefaultlibs()
 RequireRuntime()
 
 SOLUTION"gameserver"
-	
-	INCLUDES	"steamworks"
-	
+
+	includedirs{STEAMWORKS_SDK..'/public'}
+	includedirs{STEAMWORKS_SDK..'/public/steam'}
+
 	INCLUDES	"gmod_sdk"
-	INCLUDES	"source_sdk"
-	
-	--defines		{"DEBUG"}
-	defines		{"GMOD_ALLOW_DEPRECATED"}
-	
+
+	includedirs(SOURCE_SDK_INCLUDES)
+
+
+
+	defines {"GMOD_ALLOW_DEPRECATED", "GMOD_USE_SOURCESDK"}
+
 	WINDOWS()
 	LINUX()
 
 	PROJECT()
+		-- we have to manually define these because source-sdk now comes with a steam_api.lib
+		-- that doesn't contain some symbols we need, so we use steamworks' first instead
+		libdirs	{
+			STEAMWORKS_SDK..'/redistributable_bin',
+			STEAMWORKS_SDK..'/redistributable_bin/linux32'
+		}
+
+		libdirs{
+			SOURCE_SDK.."/lib/public/linux32",
+			SOURCE_SDK.."/lib/linux32",
+			SOURCE_SDK.."/lib/public"
+		}
+
 		SOURCE_SDK_LINKS()
+
 		configuration 		"windows"
-			links	"steam_api"
+			links "steam_api"
+			links "legacy_stdio_definitions" -- fixes some printf link issues
 			links		{"Ws2_32"}
 		configuration 		"linux"
